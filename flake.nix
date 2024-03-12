88d111029976cefc921e289850800a422c6d7a1a
sundayoke <sunday.oke@gmail.com>
2024-03-12 21:10:04 +0100

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,29 @@
+#sudo nixos-rebuild switch --flake /path/to/your/flake#your-hostname
+#sudo nixos-rebuild switch --flake /home/sunday/.dotfiles/.#peak-nixos
+# If you are in the .dotfiles folder
+# sudo nixos-rebuild switch --flake .#peak-nixos
+{
+  description = "My first flake!";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+
+    # use the following for unstable:
+    # nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    # or any branch you want:
+    # nixpkgs.url = "nixpkgs/{BRANCH-NAME}";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        peak-nixos = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
